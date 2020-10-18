2b1506b0585da690a8e93fc448a2843d6f66375a
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-10-18 23:25:07 +0900

    シートの取得を実装

--- src/BookManager.ts
+++ src/BookManager.ts
@@ -1,8 +1,11 @@
 var SheetID = '1stX9PL4QHZSwgKOExJ8LvRp3ZX9obRqY6b83fsIARTg'; // スプレッドシートID
+var SheetName = 'ISBN'; // シートの名前
 
 function myFunction(){
     Logger.log("Hello World!");
 
     var ss = SpreadsheetApp.openById(SheetID);
     Logger.log(ss.getName());
+    var sheet = ss.getSheetByName(SheetName);
+    Logger.log(sheet.getSheetValues(1,1,1,1));
 }
