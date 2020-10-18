2e138a43070c949420c62c90bf7127295b6fe650
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-10-18 23:22:28 +0900

    スプレッドシートの取得を実装

--- src/BookManager.ts
+++ src/BookManager.ts
@@ -1,3 +1,8 @@
+var SheetID = '1stX9PL4QHZSwgKOExJ8LvRp3ZX9obRqY6b83fsIARTg'; // スプレッドシートID
+
 function myFunction(){
     Logger.log("Hello World!");
+
+    var ss = SpreadsheetApp.openById(SheetID);
+    Logger.log(ss.getName());
 }
