7791d1b8c91c498c7fca720496de42b4c5f7ce26
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-10-21 23:17:19 +0900

    WebAPIの実行を実装

--- src/GetNDL.js
+++ src/GetNDL.js
@@ -2,9 +2,11 @@ var NDL_URL = 'https://iss.ndl.go.jp/api/sru?operation=searchRetrieve&maximumRec
 
 function GetNDL(isbn){
     var cql = "isbn=\"" + isbn + "\"";
-    Logger.log(cql);
+    //Logger.log(cql);
     var encode = encodeURI(cql);
-    Logger.log(encode);
+    //Logger.log(encode);
     var url = NDL_URL + encode;
     Logger.log(url);
+    var query = UrlFetchApp.fetch(url);
+    Logger.log(query.getContent());
 }
