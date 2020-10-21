28428f753c94ee14ecc6ff1e8061c224aff4d7a3
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-10-21 21:21:06 +0900

    URLの生成を実装

--- src/GetNDL.js
+++ src/GetNDL.js
@@ -1,6 +1,10 @@
+var NDL_URL = 'https://iss.ndl.go.jp/api/sru?operation=searchRetrieve&maximumRecords=5&query='
+
 function GetNDL(isbn){
     var cql = "isbn=\"" + isbn + "\"";
     Logger.log(cql);
     var encode = encodeURI(cql);
     Logger.log(encode);
+    var url = NDL_URL + encode;
+    Logger.log(url);
 }
