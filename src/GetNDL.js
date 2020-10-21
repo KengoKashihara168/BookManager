62f5c535c1afcde8ce7c159321f869b9995e5a9f
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-10-21 23:40:07 +0900

    URLの生成処理を関数化

--- src/GetNDL.js
+++ src/GetNDL.js
@@ -1,12 +1,21 @@
 var NDL_URL = 'https://iss.ndl.go.jp/api/sru?operation=searchRetrieve&maximumRecords=5&query='
 
 function GetNDL(isbn){
+    // APIのURL
+    var url = CreateURL(isbn);
+    
+    var query = UrlFetchApp.fetch(url);
+    //Logger.log(query.getBlob());
+    var contentText = query.getContentText();
+    
+}
+
+// URLの生成
+function CreateURL(isbn){
     var cql = "isbn=\"" + isbn + "\"";
     //Logger.log(cql);
     var encode = encodeURI(cql);
     //Logger.log(encode);
     var url = NDL_URL + encode;
-    Logger.log(url);
-    var query = UrlFetchApp.fetch(url);
-    Logger.log(query.getContent());
+    Logger.log(url + "を生成");
 }
